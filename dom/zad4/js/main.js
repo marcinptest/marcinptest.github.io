b56130f85f040baedae87bf34e1efe4dad35a956
marcinptest <114985697+marcinptest@users.noreply.github.com>
2022-11-11 15:39:57 +0100

    zadanie 4

---
+++ dom/zad4/js/main.js
@@ -0,0 +1,17 @@
+console.log("Test");
+
+const submitForm = (event) => {
+
+    event.preventDefault();
+
+let firstname = document.querySelector('[name="fname"]');
+let lastname = document.querySelector('[name="lname"]');
+
+    console.log(`Imię: ${firstname.value}, Nazwisko: ${lastname.value}`);
+
+}
+
+
+let form = document.getElementById(`form`);
+
+form.addEventListener(`submit`, submitForm);
