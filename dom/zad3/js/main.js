d829b264ce643f561468bfdc624673103e6251fe
marcinptest <114985697+marcinptest@users.noreply.github.com>
2022-11-11 14:54:42 +0100

    new

---
+++ dom/zad3/js/main.js
@@ -0,0 +1,13 @@
+const setBackground = () => {
+    let p1 = document.querySelector(`.first`);
+    let p2 = document.querySelector(`.last`);
+    
+    p1.style.backgroundColor = `red`;
+    p2.style.backgroundColor = `yellow`;
+
+
+}
+
+let btnSetBackground = document.getElementById(`set-background`);
+
+btnSetBackground.addEventListener(`click`, setBackground);
