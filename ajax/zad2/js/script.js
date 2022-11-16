eb4bce6831bc6bb1a0dae7fa8353e6cd3ad4e37a
marcinptest <114985697+marcinptest@users.noreply.github.com>
2022-11-16 20:31:58 +0100

    zadanie 2

---
+++ ajax/zad2/js/script.js
@@ -0,0 +1,32 @@
+let btnGetData = document.getElementById('get-data');
+
+const getData = () => {
+
+    fetch('https://akademia108.pl/api/ajax/get-post.php')
+    .then(res => res.json())
+    .then(data => {
+        let  pId = document.createElement('p');
+        let  pUserId = document.createElement('p');
+        let  pTitle = document.createElement('p');
+        let  pBody = document.createElement('p');
+        let hr = document.createElement('hr');
+
+
+        pId.innerText = `Post Id: ${data.id}`;
+        pUserId.innerText = `User Id: ${data.pUserId}`;
+        pTitle.innerText = `Title: ${data.title}`;
+        pBody.innerText = `Body: ${data.body}`;
+
+        document.body.appendChild(pId);
+        document.body.appendChild(pUserId);
+        document.body.appendChild(pTitle);
+        document.body.appendChild(pBody);
+        document.body.appendChild(hr);
+           })
+           .catch(error => {
+            console.error(error);
+           })
+
+}
+
+btnGetData.addEventListener('click', getData)
